f6486f3581f1fb1ac4757976653607a66e817a9b
Fathimath Shamrin Moosa Aboobacker <fathimathshamrin.dxb@gmail.com>
2025-05-26 09:52:10 +0400

    Update sql_project_sample_data.sql

--- sql_project_sample_data.sql
+++ sql_project_sample_data.sql
@@ -229,3 +229,15 @@ INSERT INTO Bookings (BookingID, PassengerID, FlightID, BookingDate, SeatClass, 
 (50, 79, 110, '2025-04-29', 'Business', 1400.00);
 
 
+ALTER TABLE Flights
+ADD COLUMN ActualArrivalTime DATETIME;
+UPDATE Flights SET ActualArrivalTime = '2025-06-01 13:05:00' WHERE FlightID = 101;  -- 5 min late
+UPDATE Flights SET ActualArrivalTime = '2025-06-01 21:50:00' WHERE FlightID = 102;  -- 10 min early
+UPDATE Flights SET ActualArrivalTime = '2025-06-02 10:45:00' WHERE FlightID = 103;  -- on time
+UPDATE Flights SET ActualArrivalTime = '2025-06-02 10:15:00' WHERE FlightID = 104;  -- 15 min late
+UPDATE Flights SET ActualArrivalTime = '2025-06-03 18:20:00' WHERE FlightID = 105;  -- 10 min early
+UPDATE Flights SET ActualArrivalTime = '2025-06-04 05:10:00' WHERE FlightID = 106;  -- 10 min late
+UPDATE Flights SET ActualArrivalTime = '2025-06-04 14:25:00' WHERE FlightID = 107;  -- 5 min late
+UPDATE Flights SET ActualArrivalTime = '2025-06-04 15:40:00' WHERE FlightID = 108;  -- 10 min late
+UPDATE Flights SET ActualArrivalTime = '2025-06-05 18:55:00' WHERE FlightID = 109;  -- 5 min early
+UPDATE Flights SET ActualArrivalTime = '2025-06-05 12:30:00' WHERE FlightID = 110;  -- 30 min late (not on time)
